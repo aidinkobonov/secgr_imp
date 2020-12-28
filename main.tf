c927c533c626916678e96d2d790f0cb21e6e351c
aidinkobonov <aidinkobonov@gmail.com>
2020-12-27 23:12:11 -0600

    ...

--- main.tf
+++ main.tf
@@ -34,21 +34,21 @@ module "http_sg" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "ticketsecgr" {
-  name        = "ticketsecgr"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "ticketsecgr" {
+#   name        = "ticketsecgr"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {    
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0./0"]
-  }
+#   ingress {    
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0./0"]
+#   }
 
-  ingress {    
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0./0"]
-  }
-}
+#   ingress {    
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0./0"]
+#   }
+# }

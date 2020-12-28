7f96e1eeb39b37e3f369d96f3d3080f58545f728
aidinkobonov <aidinkobonov@gmail.com>
2020-12-27 23:23:39 -0600

    ...

--- secgroup.tf
+++ secgroup.tf
@@ -13,6 +13,6 @@ resource "aws_security_group" "ticketsecgr" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0./0"]
+    cidr_blocks = ["0.0.0.0."]
   }
 }
